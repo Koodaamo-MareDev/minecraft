e0894165683f7e1f6c212340f08163f6ff17e4ec
MareDev <marcus.erkkila.me@gmail.com>
2024-04-09 18:36:09 +0300

    Calculate elapsed time as double for accuracy.

--- source/timers.hpp
+++ source/timers.hpp
@@ -19,7 +19,7 @@ inline int64_t time_diff_ms(uint64_t f, uint64_t s) {
 }
 
 inline float time_diff_s(uint64_t f, uint64_t s) {
-	return (float)(s - f) / (float)(1000UL * TB_TIMER_CLOCK);
+	return float(double(s - f) / double(1000UL * TB_TIMER_CLOCK));
 }
 
 #endif
