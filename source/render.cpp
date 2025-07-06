3a955ca42d7cebfee22aed3b7d52b15bb033e346
Marcus <20874339+Koodaamo-MareDev@users.noreply.github.com>
2025-07-06 14:12:37 +0300

    Fix fov margin typo

--- source/render.cpp
+++ source/render.cpp
@@ -581,7 +581,7 @@ frustum_t calculate_frustum(camera_t &camera)
 {
     frustum_t frustum;
 
-    constexpr float fov_margin = 15.0f; // Used to prevent overculling of blocks at the edges of the screen
+    constexpr float fov_margin = 10.0f; // Used to prevent overculling of blocks at the edges of the screen
 
     // Calculate half-width and half-height at near plane
     float half_fov = camera.fov * 0.5f;
@@ -592,9 +592,9 @@ frustum_t calculate_frustum(camera_t &camera)
 
     // Calculate the 4 perspective frustum planes (not near and far)
     guVector right_vec = -angles_to_vector(camera.rot.x, camera.rot.y + 90 + half_fov + fov_margin);
-    guVector left_vec = -angles_to_vector(camera.rot.x, camera.rot.y - 90 - half_fov);
-    guVector up_vec = -angles_to_vector(camera.rot.x + 90 + half_fov, camera.rot.y);
-    guVector down_vec = -angles_to_vector(camera.rot.x - 90 - half_fov, camera.rot.y);
+    guVector left_vec = -angles_to_vector(camera.rot.x, camera.rot.y - 90 - half_fov + fov_margin);
+    guVector up_vec = -angles_to_vector(camera.rot.x + 90 + half_fov + fov_margin, camera.rot.y);
+    guVector down_vec = -angles_to_vector(camera.rot.x - 90 - half_fov + fov_margin, camera.rot.y);
 
     // Calculate points on the near and far planes
     guVector near_center = (vec3f(forward) * (camera.near));
