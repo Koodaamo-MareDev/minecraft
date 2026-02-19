0a36129b11bb4345870fa23ca6120d963d53c3b3
Marcus <20874339+Koodaamo-MareDev@users.noreply.github.com>
2026-02-19 18:07:10 +0200

    Fix time offset in SystemTime.hpp and improve documentation.

--- source/ported/SystemTime.hpp
+++ source/ported/SystemTime.hpp
@@ -8,11 +8,13 @@ namespace javaport
     class System
     {
     public:
+        // Converts Wii time to Unix timestamp and returns it in milliseconds.
         static int64_t currentTimeMillis()
         {
-            return ticks_to_millisecs(SYS_Time());
+            return ticks_to_millisecs(SYS_Time()) + 946684800000ULL;
         }
 
+        // NOTE: This doesn't reflect the absolute time. Should be only used for calculating elapsed time.
         static int64_t nanoTime()
         {
             return ticks_to_nanosecs(SYS_Time());
